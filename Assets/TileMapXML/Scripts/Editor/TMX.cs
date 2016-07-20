d181d1dc13544ea14995f42daf135cba0f176b41
James LaFritz <renegadejl@hotmail.com>
2016-07-20 02:29:40 -0400

    The tiled map xml loading in unity tutorial and completed TMX class

--- Assets/TileMapXML/Scripts/Editor/TMX.cs
+++ Assets/TileMapXML/Scripts/Editor/TMX.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Xml.Serialization;
+using System.IO;
 
 namespace TileMapXML
 {
+    /// <summary>
+    /// Loads in a map created in Tiled using xml serialization.
+    /// Stores the map in a variable called map
+    /// </summary>
     public class TMX
     {
+        /// <summary>
+        /// The tmx map
+        /// </summary>
+        public TMXMap map;
+
+        /// <summary>
+        /// Loads A TMX file from xml using serialization
+        /// </summary>
+        /// <param name="tmxFilePath">The path of the file to load</param>
+        public void Load(string tmxFilePath)
+        {
+            // Load the map from the xml file at tmxFilePath
+            XmlSerializer serializer = new XmlSerializer(typeof(TMXMap));
+            using(FileStream stream = new FileStream(tmxFilePath, FileMode.Open))
+                map = serializer.Deserialize(stream) as TMXMap;
+        }//public void Load
     }//public class TMX
 }//namespace TileMapXML
