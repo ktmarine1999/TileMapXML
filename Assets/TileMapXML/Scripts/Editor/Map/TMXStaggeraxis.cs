a164c496d008f32dc736230a11e0c6f8d7bffe1e
James LaFritz <renegadejl@hotmail.com>
2016-07-26 15:40:45 -0400

    Tiled Map Format added

---
+++ Assets/TileMapXML/Scripts/Editor/Map/TMXStaggeraxis.cs
@@ -0,0 +1,14 @@
+﻿namespace TileMapXML.Map
+{
+    /// <summary>
+    /// staggeraxis: For staggered and hexagonal maps, 
+    ///         determines which axis("x" or "y") is staggered. (since 0.11)
+    /// </summary>
+    [System.Serializable]
+    public enum TMXStaggerAxis
+    {
+        none,
+        x,
+        y
+    }//enum TMXMapStaggerAxis
+}//namespace TileMapXML.Map
